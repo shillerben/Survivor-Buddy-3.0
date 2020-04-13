759e86a9a3387fda3ef7a61723a225214059a03a
ptrett <ptrett@tamu.edu>
2020-04-12 23:51:45 -0500

    Made significant changes to render, still WIP

--- gui/PositionFrame.py
+++ gui/PositionFrame.py
@@ -15,29 +15,37 @@ import numpy as np
 from threading import Thread
 import time
 
-
 class PositionUpdater(Thread):
-    def __init__(self, dev, _pitch_control, _yaw_control, _roll_control, **kwargs):
+    def __init__(self, dev, _pitch_control, _yaw_control, _roll_control, _pos_render, **kwargs):
         super().__init__(**kwargs)
         self.serial_arm_controller = dev
         self.pitch_control = _pitch_control
         self.yaw_control = _yaw_control
         self.roll_control = _roll_control
+        self.pos_render = _pos_render
 
     def run(self):
         while True:
             time.sleep(0.1)
-            self.serial_arm_controller.update_position()
-            pitch = self.serial_arm_controller.position.pitch
-            yaw = self.serial_arm_controller.position.yaw
-            roll = self.serial_arm_controller.position.roll
-            self.pitch_control.slider.set(pitch)
-            self.pitch_control.spinbox.set(pitch)
-            self.yaw_control.slider.set(yaw)
-            self.yaw_control.spinbox.set(yaw)
-            self.roll_control.slider.set(roll)
-            self.roll_control.spinbox.set(roll)
+            if self.serial_arm_controller.is_connected: #If else to allow testing of GUI without connected arm
+                self.serial_arm_controller.update_position()
+                self.pitch_control.slider.set(pitch)
+                self.pitch_control.spinbox.set(pitch)
+                self.yaw_control.slider.set(yaw)
+                self.yaw_control.spinbox.set(yaw)
+                self.roll_control.slider.set(roll)
+                self.roll_control.spinbox.set(roll)
 
+                pitch = self.serial_arm_controller.position.pitch
+                yaw = self.serial_arm_controller.position.yaw
+                roll = self.serial_arm_controller.position.roll
+            else:
+                pitch = self.pitch_control.spinbox.get()
+                yaw = self.yaw_control.spinbox.get()
+                roll = self.roll_control.spinbox.get()
+                
+            # self.pos_render.update_render()   #NEEDS FIXING
+            
 
 class LabelScaleSpinbox(tk.Frame):
     def __init__(self, master, text="", from_=0, to=10, axis=0, dev=None, **kwargs):
@@ -79,7 +87,6 @@ class LabelScaleSpinbox(tk.Frame):
         self.spinbox.set(str(round(self.current_value)))
         
     def sliderUpdate(self, val):
-        print('Slider Update')
         newVal = int(self.slider.get())
         self.spinbox.set(newVal)    #Update spinbox value
         self.current_value = newVal
@@ -115,13 +122,50 @@ class LabelScaleSpinbox(tk.Frame):
     def send_command(self):
         if self.axis == 0:  #Pitch
             self.serial_arm_controller.set_pitch(self.current_value) if self.serial_arm_controller.is_connected else print('Device Disconnected')
-            PositionFrame.pitch = self.current_value
         elif self.axis == 1:    #Yaw
             self.serial_arm_controller.set_yaw(self.current_value) if self.serial_arm_controller.is_connected else print('Device Disconnected')
-            PositionFrame.yaw = self.current_value
         elif self.axis == 2:    #Roll
             self.serial_arm_controller.set_roll(self.current_value) if self.serial_arm_controller.is_connected else print('Device Disconnected')   
-            PositionFrame.roll = self.current_value
+        
+
+class RenderDiagram(tk.Frame): 
+    def __init__(self, master, dev=None, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.serial_arm_controller = dev
+
+        # Disable plot toolbar
+        mpl.rcParams['toolbar'] = 'None'
+
+        # Set up 3d plot, define size
+        self.fig = plt.figure(figsize=(3,3))
+        self.ax = self.fig.gca(projection='3d')
+
+        # Remove unneccesary information from plot
+        self.ax.set_xticklabels([])
+        self.ax.set_yticklabels([])
+        self.ax.set_zticklabels([])
+
+        # Draw x, y and z axes to make plot easier to read, set plot size
+        self.ax.quiver(-2, 0, 0, 4, 0, 0, length=1.0, arrow_length_ratio=0, color = '#cf685d')
+        self.ax.quiver(0, -2, 0, 0, 4, 0, length=1.0, arrow_length_ratio=0, color = '#5d5fcf')
+        self.ax.quiver(0, 0, -2, 0, 0, 4, length=1.0, arrow_length_ratio=0, color = '#6ad15e')
+        self.ax.set_xlim(left=-2, right=2, emit=True, auto=False)
+        self.ax.set_ylim(bottom=-2, top=2, emit=True, auto=False)
+        self.ax.set_zlim(bottom=-2, top=2, emit=True, auto=False)
+
+        self.render_canvas = FigureCanvasTkAgg(self.fig, master)
+        self.render_canvas.get_tk_widget().pack()
+    
+    def update_render(self):    #Should add an arbitrary line to render, would change to be accurate to position, but line does not appear
+        # print('Update Render\n')
+        self.ax.quiver(0, 0, 0, 1, 2, 3, length=10.0, arrow_length_ratio=0)
+
+    # def update_yaw(self, newYaw):   #will implement if I can figure out how to update plot
+
+    # def update_pitch(self, newPitch):
+
+    # def update_roll(self, newRoll):
 
 class PositionFrame(tk.Frame):
     def __init__(self, master, arm_controller, **kwargs):
@@ -136,36 +180,14 @@ class PositionFrame(tk.Frame):
         self.control_frame = tk.Frame(self)
         self.control_frame.pack(side="left")
         self.create_controls(self.control_frame)
-        self.yaw = 100
-        self.pitch = 100
-        self.roll = 100
+
+        self.create_updater()
         
-    def create_render(self, master):      
-        # Disable plot toolbar
-        mpl.rcParams['toolbar'] = 'None'
-
-        # Set up 3d plot, define size
-        fig = plt.figure(figsize=(3,3))
-        ax = fig.gca(projection='3d')
-
-        # Remove unneccesary information from plot
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_zticklabels([])
-        ax.legend().remove()
-
-        # Draw x, y and z axes to make plot easier to read, set plot size
-        ax.quiver(-2, 0, 0, 4, 0, 0, length=1.0, arrow_length_ratio=0, color = '#cf685d')
-        ax.quiver(0, -2, 0, 0, 4, 0, length=1.0, arrow_length_ratio=0, color = '#5d5fcf')
-        ax.quiver(0, 0, -2, 0, 0, 4, length=1.0, arrow_length_ratio=0, color = '#6ad15e')
-        ax.set_xlim(left=-2, right=2, emit=True, auto=False)
-        ax.set_ylim(bottom=-2, top=2, emit=True, auto=False)
-        ax.set_zlim(bottom=-2, top=2, emit=True, auto=False)
-
-        ax.quiver(0, 0, 0, 1, 1, 0, length=1.0)
-
-        self.render_canvas = FigureCanvasTkAgg(fig, master)
-        self.render_canvas.get_tk_widget().pack()
+    def create_render(self, master):    
+        self.pos_render = RenderDiagram(
+            master, dev=self.serial_arm_controller
+        )
+        self.pos_render.pack()
         
     def create_controls(self, master):
         self.pitch_control = LabelScaleSpinbox(
@@ -180,11 +202,14 @@ class PositionFrame(tk.Frame):
             master, text="Roll: ", from_=0, to=90, axis=2, dev=self.serial_arm_controller)
         self.roll_control.pack()
 
+    def create_updater(self):   #Updater for sliders and render
         self.update_thread = PositionUpdater(
             self.serial_arm_controller,
             self.pitch_control,
             self.yaw_control,
-            self.roll_control
+            self.roll_control,
+            self.pos_render
         )
         self.update_thread.start()
+
         
