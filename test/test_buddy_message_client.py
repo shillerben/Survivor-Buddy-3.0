4256ae97cc8832459bd4cd6b527936e404eb49b3
khutes <kyle.hutto3@gmail.com>
2020-11-01 12:40:35 -0600

    Fixed wrong name on test class

--- test/test_buddy_message_client.py
+++ test/test_buddy_message_client.py
@@ -3,7 +3,7 @@ import pytest
 from gui.BuddyMessageClient import BuddyMessageClient
 
 
-class TestBuddyAudioClientPositive:
+class TestBuddyMessageClientPositive:
 
     def test_foo(self):
         assert True
