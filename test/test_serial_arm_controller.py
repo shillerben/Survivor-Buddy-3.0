c6bc81e6f2004e833de4268efc4fedd6f967a421
khutes <kyle.hutto3@gmail.com>
2020-11-01 12:29:32 -0600

    Reorganized tests into test class with shared mock variables for code brevity

--- test/test_serial_arm_controller.py
+++ test/test_serial_arm_controller.py
@@ -2,152 +2,166 @@ import pytest
 from Mock import MockSerial, MockNotificationsFrame, MockStatusBar
 from gui.SerialArmController import SerialArmController, Command, Position
 
-mock_serial = None
-mock_notif_frame = None
-mock_status_bar = None
-my_sar = None
+"""
+self.mock_serial = None
+self.mock_notif_frame = None
+self.mock_status_bar = None
+self.my_sar = None
 
-def reset_mocked_sar():
-    global mock_serial
-    global mock_notif_frame
-    global mock_status_bar
-    global my_sar
+def self.reset_mocked_sar():
+    global self.mock_serial
+    global self.mock_notif_frame
+    global self.mock_status_bar
+    global self.my_sar
     
-    mock_serial = MockSerial()
-    mock_notif_frame = MockNotificationsFrame()
-    mock_status_bar = MockStatusBar()
-    my_sar = SerialArmController(mock_status_bar, mock_notif_frame)
-    my_sar._device = mock_serial
+    self.mock_serial = MockSerial()
+    self.mock_notif_frame = MockNotificationsFrame()
+    self.mock_status_bar = MockStatusBar()
+    self.my_sar = SerialArmController(self.mock_status_bar, self.mock_notif_frame)
+    self.my_sar._device = self.mock_serial
+"""
+
+class TestSerialArmControllerPositive:
+
+    mock_serial = None
+    mock_notif_frame = None
+    mock_status_bar = None
+    my_sar = None   #SerialArmController object for testing
+
+    def reset_mocked_sar(self):
+
+        self.mock_serial = MockSerial()
+        self.mock_notif_frame = MockNotificationsFrame()
+        self.mock_status_bar = MockStatusBar()
+        self.my_sar = SerialArmController(self.mock_status_bar, self.mock_notif_frame)
+        self.my_sar._device = self.mock_serial
+
+    def test_close_connected(self):
+        """
+        Tests close() method of SerialArmController when it is connected
+        """
+
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = True  #sets sar to connected state
+
+        #run test
+        self.my_sar.close()
+
+        #check
+        assert self.mock_status_bar.get_status() == "DISCONNECTED"
+        assert self.mock_serial.open_bool == False
+        assert self.my_sar.is_connected == False
+
+    def test_close_not_connected(self):
+        """
+        Tests close() method of SerialArmController when it not connected
+        """
+
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = False
+
+        #run test
+        self.my_sar.close()
+
+        #check
+        assert self.mock_status_bar.get_status() == None
+
+    def test_send_good_bytes_connected(self):
+        """
+        Tests send() method of SerialArmController with preset byte data as input
+        while it is connected
+        """
+
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = True
+
+        #test
+        in_bytes = bytes((0,1,2,3))
+        self.my_sar.send(in_bytes)
+        out_bytes = self.mock_serial.read(in_bytes.__len__())
+
+        #check
+        assert out_bytes == in_bytes
 
 
+    def test_send_good_bytes_not_connected(self):
+        """
+        Tests send() method of SerialArmController with preset byte data as input
+        while it is not connected
+        """
+
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = False
+
+        #test
+        in_bytes = bytes((0, 1, 2, 3))
+        self.my_sar.send(in_bytes)
+        out_bytes = self.mock_serial.read(in_bytes.__len__())
+
+        #check
+        assert out_bytes == b''
 
 
-def test_close_connected():
-    """
-    Tests close() method of SerialArmController when it is connected
-    """
+    def test_recv_good_bytes_connected(self):
+        """
+        Tests recv() method of SerialArmController with preset byte data as input
+        while it is connected
+        """
 
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = True  #sets sar to connected state
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = True
 
-    #run test
-    my_sar.close()
+        #test
 
-    #check
-    assert mock_status_bar.get_status() == "DISCONNECTED"
-    assert mock_serial.open_bool == False
-    assert my_sar.is_connected == False
+        in_bytes = bytes((3,2,1))
+        self.mock_serial.write(in_bytes)
+        out_bytes = self.my_sar.recv()
 
-def test_close_not_connected():
-    """
-    Tests close() method of SerialArmController when it not connected
-    """
+        #check
+        assert out_bytes == in_bytes
 
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = False
+    def test_update_position_not_connected(self):
+        """
+        Tests update_position() method of SerialArmController with preset byte data as input
+        while it is NOT connected
+        """
 
-    #run test
-    my_sar.close()
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = False
 
-    #check
-    assert mock_status_bar.get_status() == None
-
-def test_send_good_bytes_connected():
-    """
-    Tests send() method of SerialArmController with preset byte data as input
-    while it is connected
-    """
-
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = True
-
-    #test
-    in_bytes = bytes((0,1,2,3))
-    my_sar.send(in_bytes)
-    out_bytes = mock_serial.read(in_bytes.__len__())
-
-    #check
-    assert out_bytes == in_bytes
+        #test
+        in_bytes = bytes((3, 100, 1))
+        self.mock_serial.write(in_bytes)
+        self.my_sar.update_position()
 
 
-def test_send_good_bytes_not_connected():
-    """
-    Tests send() method of SerialArmController with preset byte data as input
-    while it is not connected
-    """
-
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = False
-
-    #test
-    in_bytes = bytes((0, 1, 2, 3))
-    my_sar.send(in_bytes)
-    out_bytes = mock_serial.read(in_bytes.__len__())
-
-    #check
-    assert out_bytes == b''
+        #check
+        assert self.my_sar.position.pitch == 0
+        assert self.my_sar.position.yaw == 0
+        assert self.my_sar.position.roll == 0
 
 
-def test_recv_good_bytes_connected():
-    """
-    Tests recv() method of SerialArmController with preset byte data as input
-    while it is connected
-    """
+    @pytest.mark.parametrize("input_val", [90,63,172])
+    def test_set_pitch_connected(self, input_val):
+        """
+        Tests set_pitch() method of SerialArmController with preset byte data as input
+        while it is connected
+        """
+        
+        #setup
+        self.reset_mocked_sar()
+        self.my_sar.is_connected = True
 
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = True
+        #test
+        self.my_sar.set_pitch(input_val)
 
-    #test
-
-    in_bytes = bytes((3,2,1))
-    mock_serial.write(in_bytes)
-    out_bytes = my_sar.recv()
-
-    #check
-    assert out_bytes == in_bytes
-
-def test_update_position_not_connected():
-    """
-    Tests update_position() method of SerialArmController with preset byte data as input
-    while it is NOT connected
-    """
-
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = False
-
-    #test
-    in_bytes = bytes((3, 100, 1))
-    mock_serial.write(in_bytes)
-    my_sar.update_position()
-
-
-    #check
-    assert my_sar.position.pitch == 0
-    assert my_sar.position.yaw == 0
-    assert my_sar.position.roll == 0
-
-
-@pytest.mark.parametrize("input_val", [90,63,172])
-def test_set_pitch_connected(input_val):
-    """
-    Tests set_pitch() method of SerialArmController with preset byte data as input
-    while it is connected
-    """
-    
-    #setup
-    reset_mocked_sar()
-    my_sar.is_connected = True
-
-    #test
-    my_sar.set_pitch(input_val)
-
-    out_bytes = mock_serial.read(2)
-    assert int(out_bytes[0]) == Command.PITCH
-    assert int(out_bytes[1]) == input_val
+        out_bytes = self.mock_serial.read(2)
+        assert int(out_bytes[0]) == Command.PITCH
+        assert int(out_bytes[1]) == input_val
 
